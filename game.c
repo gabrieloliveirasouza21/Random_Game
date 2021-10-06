63aa1c515006e41f110cbd245cf78a26be851ad4
Gabriel Oliveira <gabrielosouza21@gmail.com>
2021-10-05 21:32:27 -0300

    Game Updated

--- game.c
+++ game.c
@@ -2,45 +2,55 @@
 // is not ready yet...
 
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <time.h>
 
-void rules (){
+int num, mNum;
 
-    printf ("1) CHOOSE A NUMBER (1 TO 100)\n");
-    printf ("2) THE COMPUTER WILL GENERATE A NUMBER\n");
-    printf ("3) YOU'LL HAVE TO GUESS WHICH NUMBER THE COMPUTER 'THOUGHT'\n");
-    printf ("4) THE COMPUTER WILL TELL YOU IF THE NUMBER IS HIGHER OR LOWER THAN THE NUMBER YOU GUESS\n\n");
+void game() {
+
+    while (num != mNum) {
+        if (num > mNum) {
+            printf("The number is higher, try it again : ");
+            scanf("%d", & num);
+        }
+        if (num < mNum) {
+            printf("The number is lower, try it again : ");
+            scanf("%d", & num);
+        }
+    }
+    if (num == mNum) {
+        printf("CONGRATULATIONS, YOU GUESSED !!!!!. The Number Was %d", mNum);
+    }
+
+    if (num != mNum) {
+        printf("I'm sorry, game over...");
+    }
 }
 
-int main (){
+void rules() {
 
-     int numberChosen, *a;
-     a = &numberChosen;
+    printf("1) CHOOSE A NUMBER (1 TO 100)\n");
+    printf("2) THE COMPUTER WILL GENERATE A NUMBER\n");
+    printf("3) YOU'LL HAVE TO GUESS WHICH NUMBER THE COMPUTER 'THOUGHT'\n");
+    printf("4) THE COMPUTER WILL TELL YOU IF THE NUMBER IS HIGHER OR LOWER THAN THE NUMBER YOU GUESS\n\n");
+}
 
-    printf("\n\n\n\n----------------- Welcome to the Game ! -----------------\n\n\n\n") ;
+int main() {
+
+
+
+    printf("\n\n\n\n----------------- Welcome to the Game ! -----------------\n\n\n\n");
     printf("RULES OF THE GAME : \n\n");
-    rules ();
+    rules();
 
     printf("THE COMPUTER NOW WILL THINK IN A NUMBER\n\n ");
     srand(time(NULL));
-
-    printf ("CHOOSE A NUMBER : ");
-    scanf("%d", &numberChosen);
-
-        if (numberChosen==rand()%101){
-            printf ("CONGRATULATIONS, YOU GUESSED THE NUMBER !!!!!!");
-        } 
-        while (numberChosen>rand()%101){
-            printf("THE NUMBER IS HIGHER THAN COMPUTER'S NUMBER\n");
-            printf("TRY AGAIN :");
-            scanf("%d", &numberChosen);
-        }
-        while (numberChosen<rand()%101){
-            printf("THE NUMBER IS LOWER THAN COMPUTER'S NUMBER");
-            printf("TRY AGAIN :");
-            scanf("%d", &numberChosen);
-        }
-
+    mNum = rand() % 100;
+    printf("CHOOSE A NUMBER : ");
+    scanf("%d", & num);
+    game();
     return 0;
 }
